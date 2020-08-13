581d75b81651e13eaca59724ce3a80f578321d12
Ishan <mrishan1993@gmail.com>
2020-08-13 18:20:23 +0530

    strcuture

--- controllers/witController.js
+++ controllers/witController.js
@@ -4,6 +4,7 @@ let interactive = null;
 var async = require('async');
 const config = require('../config');
 const helperFunc = require('../lib/helper');
+const _ = require('lodash');
 const axios = require('axios');
 // import wit libraries
 try {
@@ -20,9 +21,10 @@ const client = new Wit({
 
 var witController = {
   witProcessController: async function(request, h) {
-    var totalCount = 0;
-    var recipes = [];
     var returnObject = {};
+    var entity = {};
+    var entities = [];
+    var intent = {};
     var result = await client
       .message('What is cooking for dinner')
       .then(data => {
@@ -34,70 +36,28 @@ var witController = {
         console.log('error', error);
         return error;
       });
-    // do something with result
-    // some function here
-    // return async.auto(
-    //   {
-    //     one: function(callback) {
-    //       client
-    //         .message('What is cooking for dinner')
-    //         .then(data => {
-    //           console.log('data', data);
-    //           callback(null, data);
-    //           // return data;
-    //         })
-    //         .catch(error => {
-    //           console.log('error', error);
-    //           callback(error, null);
-    //         });
-    //     },
-    //   },
-    //   function(err, results) {
-    //     return results;
-    //   },
-    // );
 
-    // generate random number
-    var randomNumber = helperFunc.generateRandom(100 - 3);
-    // get 3 random recipes
-    await axios
-      .get(
-        'https://api.edamam.com/search?q=indian&app_id=' +
-          config.edamamApiID +
-          '&app_key=' +
-          config.edamamApiKey +
-          '&from=' +
-          randomNumber +
-          '&to=' +
-          (randomNumber + 3),
-      )
-      .then(function(response) {
-        // handle success
-        console.log(response);
-        if (
-          response.data &&
-          response.data.hits &&
-          response.data.hits.length > 0
-        ) {
-          recipes = response.data.hits;
-          returnObject = {
-            success: true,
-            data: recipes,
-            status: 200,
-          };
-        }
-      })
-      .catch(function(error) {
-        // handle error
-        console.log(error);
-        returnObject = {
-          success: false,
-          error: error.message,
-        };
-      })
-      .then(function() {
-        // always executed
+    // find the intent with max confidence
+
+    if (result && result.intents && result.intents.length > 0) {
+      intent = _.maxBy(result.intents, function(o) {
+        return o.confidence;
       });
+    }
+
+    if (result && result.entities) {
+      _.forEach(result.entities, function(value, key) {
+        entities.push(value);
+      });
+    }
+    entities = _.flatten(entities);
+    if (entities && entities.length > 0) {
+      entity = _.maxBy(entities, function(o) {
+        return o.confidence;
+      });
+    }
+    returnObject = await handleIntents(intent, entity);
+
     // const movie = await request.mongo.db
     //   .collection('listingsAndReviews')
     //   .find({})
@@ -105,6 +65,74 @@ var witController = {
     //   .toArray();
     return returnObject;
   },
+};
+
+var handleIntents = (intent, entity) => {
+  var returnObject = {};
+  switch (intent.name) {
+    case 'Get_Recipe':
+      returnObject = handleGetRecipe(intent, entity);
+    default:
+  }
+  return returnObject;
+};
+
+var handleGetRecipe = async (intent, entity) => {
+  // generate random number
+  var randomNumber = helperFunc.generateRandom(100 - 3);
+  var mealType = '';
+  var returnObject = {};
+  var recipes = [];
+  var q = 'indian';
+  // get 3 random recipes
+  if (entity.name === 'cook_time') {
+    mealType = entity.value;
+  } else {
+    q = entity.value;
+  }
+  await axios
+    .get(
+      'https://api.edamam.com/search?q=' +
+        q +
+        '&app_id=' +
+        config.edamamApiID +
+        '&app_key=' +
+        config.edamamApiKey +
+        '&from=' +
+        randomNumber +
+        '&to=' +
+        (randomNumber + 3) +
+        '&Health=' +
+        'vegetarian',
+    )
+    .then(function(response) {
+      // handle success
+      console.log(response);
+      if (
+        response.data &&
+        response.data.hits &&
+        response.data.hits.length > 0
+      ) {
+        recipes = response.data.hits;
+        returnObject = {
+          success: true,
+          data: recipes,
+          status: 200,
+        };
+      }
+    })
+    .catch(function(error) {
+      // handle error
+      console.log(error);
+      returnObject = {
+        success: false,
+        error: error.message,
+      };
+    })
+    .then(function() {
+      // always executed
+    });
+  return returnObject;
 };
 
 module.exports = witController;
